70ffec05cd8c6e8fc0a5b1ae9d2a74ef09ac3339
kaestner <kaestner@c96eeb0f-4990-4d45-9600-2c6bf17e0709>
2013-06-07 09:49:46 +0000

    Nearly implemented the config info transfer

--- src/muhrec3/muhrecmainwindow.cpp
+++ src/muhrec3/muhrecmainwindow.cpp
@@ -69,6 +69,17 @@ void MuhRecMainWindow::UpdateDialog()
     ui->checkCorrectTilt->setChecked(m_Config.ProjectionInfo.bCorrectTilt);
     ui->moduleconfigurator->SetModules(m_Config.modules);
     ui->dspinRotateRecon->setValue(m_Config.MatrixInfo.fRotation);
+    ui->dspinGrayLow->setValue(m_Config.MatrixInfo.fGrayInterval[0]);
+    ui->dspinGrayHigh->setValue(m_Config.MatrixInfo.fGrayInterval[1]);
+    ui->checkUseMatrixROI->setChecked(m_Config.MatrixInfo.bUseROI);
+    ui->spinMatrixROI0->setValue(m_Config.MatrixInfo.roi[0]);
+    ui->spinMatrixROI1->setValue(m_Config.MatrixInfo.roi[1]);
+    ui->spinMatrixROI2->setValue(m_Config.MatrixInfo.roi[2]);
+    ui->spinMatrixROI3->setValue(m_Config.MatrixInfo.roi[3]);
+    ui->editDestPath->setText(QString::fromStdString(m_Config.MatrixInfo.sDestinationPath));
+    ui->editSliceMask->setText(QString::fromStdString(m_Config.MatrixInfo.sFileMask));
+    ui->comboDestFileType->setCurrentIndex(m_Config.MatrixInfo.FileType);
+  //  ui->logviewer->SetLogLevel(m_Config.System.eLogLevel);
 
     str.str("");
     std::set<size_t>::iterator it;
@@ -78,4 +89,60 @@ void MuhRecMainWindow::UpdateDialog()
 }
 
 void MuhRecMainWindow::UpdateConfig()
-{}
+{
+    m_Config.ProjectionInfo.sPath     = ui->editProjectionPath->text().toStdString();
+    m_Config.ProjectionInfo.sFileMask = ui->editProjectionMask->text().toStdString();
+    m_Config.ProjectionInfo.nFirstIndex = ui->spinFirstProjection->value();
+    m_Config.ProjectionInfo.nLastIndex = ui->spinLastProjection->value();
+    m_Config.ProjectionInfo.nProjectionStep = ui->spinProjectionStep->value();
+    m_Config.ProjectionInfo.imagetype = static_cast<ReconConfig::cProjections::eImageType>(ui->comboProjectionStyle->currentIndex());
+    m_Config.ProjectionInfo.fBinning = ui->spinProjectionBinning->value();
+    m_Config.ProjectionInfo.eFlip = static_cast<kipl::base::eImageFlip>(ui->comboFlipProjection->currentIndex());
+    m_Config.ProjectionInfo.eRotate = static_cast<kipl::base::eImageRotate>(ui->comboRotateProjection->currentIndex());
+
+    m_Config.ProjectionInfo.sReferencePath = ui->editReferencePath->text().toStdString();
+    m_Config.ProjectionInfo.sOBFileMask = ui->editOpenBeamMask->text().toStdString();
+    m_Config.ProjectionInfo.nOBFirstIndex = ui->spinFirstOpenBeam->value();
+    m_Config.ProjectionInfo.nOBCount = ui->spinOpenBeamCount->value();
+    m_Config.ProjectionInfo.sDCFileMask = ui->editDarkMask->text().toStdString();
+    m_Config.ProjectionInfo.nDCFirstIndex = ui->spinFirstDark->value();
+    m_Config.ProjectionInfo.nDCCount = ui->spinDarkCount->value();
+
+    m_Config.ProjectionInfo.dose_roi[0] = ui->spinDoseROIx0->value();
+    m_Config.ProjectionInfo.dose_roi[1] = ui->spinDoseROIy0->value();
+    m_Config.ProjectionInfo.dose_roi[2] = ui->spinDoseROIx1->value();
+    m_Config.ProjectionInfo.dose_roi[3] = ui->spinDoseROIy1->value();
+
+    m_Config.ProjectionInfo.roi[0] = ui->spinMarginLeft->value();
+    m_Config.ProjectionInfo.roi[2] = ui->spinMarginRight->value();
+    m_Config.ProjectionInfo.roi[1] = ui->spinSlicesFirst->value();
+    m_Config.ProjectionInfo.roi[3] = ui->spinSlicesLast->value();
+
+    m_Config.ProjectionInfo.fCenter = ui->dspinRotationCenter->value();
+    m_Config.ProjectionInfo.fScanArc[0] = ui->dspinAngleStart->value();
+    m_Config.ProjectionInfo.fScanArc[1] = ui->dspinAngleStop->value();
+    m_Config.ProjectionInfo.scantype = static_cast<ReconConfig::cProjections::eScanType>(ui->comboDataSequence->currentIndex());
+    m_Config.ProjectionInfo.fResolution[0] = ui->dspinResolution->value();
+    m_Config.ProjectionInfo.fTiltAngle = ui->dspinTiltAngle->value();
+    m_Config.ProjectionInfo.fTiltPivotPosition = ui->dspinTiltPivot->value();
+    m_Config.ProjectionInfo.bCorrectTilt = ui->checkCorrectTilt->checkState();
+    m_Config.modules = ui->moduleconfigurator->GetModules();
+    m_Config.MatrixInfo.fRotation= ui->dspinRotateRecon->value();
+    m_Config.MatrixInfo.fGrayInterval[0] = ui->dspinGrayLow->value();
+    m_Config.MatrixInfo.fGrayInterval[1] = ui->dspinGrayHigh->value();
+    m_Config.MatrixInfo.bUseROI = ui->checkUseMatrixROI->checkState();
+    m_Config.MatrixInfo.roi[0] = ui->spinMatrixROI0->value();
+    m_Config.MatrixInfo.roi[1] = ui->spinMatrixROI1->value();
+    m_Config.MatrixInfo.roi[2] = ui->spinMatrixROI2->value();
+    m_Config.MatrixInfo.roi[3] = ui->spinMatrixROI3->value();
+    m_Config.MatrixInfo.sDestinationPath = ui->editDestPath->text().toStdString();
+    m_Config.MatrixInfo.sFileMask = ui->editSliceMask->text().toStdString();
+    m_Config.MatrixInfo.FileType = static_cast<kipl::io::eFileType>(ui->comboDestFileType->currentIndex());
+
+
+//    str.str("");
+//    std::set<size_t>::iterator it;
+//    for (it=m_Config.ProjectionInfo.nlSkipList.begin(); it!=m_Config.ProjectionInfo.nlSkipList.end(); it++)
+//        str<<*it<<" ";
+//    ui->editProjectionSkipList->setText(QString::fromStdString(str.str()));
+}
