b9ec865fc3b08bef34c6e60682b3bb1f560bcc4f
Anders Kaestner <anderskaestner@users.noreply.github.com>
2013-11-13 15:56:57 +0000

    Updated the copy script

--- copy_dylibs.sh
+++ copy_dylibs.sh
@@ -1,29 +1,39 @@
 #CPCMD="ln -s "
 CPCMD="cp "
-DEST="imagingtool-build-desktop-Desktop_Qt_4_8_1_for_GCC__Qt_SDK__Release/imagingtool.app/Contents/MacOS"
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   DEST="imagingtool-build-desktop-Qt_4_8_1_in_PATH__System__Release/"
+   REPOSPATH="/home/kaestner/repos"
+   EXT="so.1.0.0"
+   EXT1="so.1.0"
+   EXT2="so.1"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   DEST="imagingtool-build-desktop-Desktop_Qt_4_8_1_for_GCC__Qt_SDK__Release/imagingtool.app/Contents/MacOS"
+   REPOSPATH="/Users/kaestner/repos"
+   EXT="1.0.0.dylib"
+   EXT1="1.0.dylib"
+   EXT2="1.dylib"
+fi
+
 pushd .
 cd $DEST
-rm -f *.1.0.0.dylib
+rm -f *.$EXT
 
-#`$CPCMD /Users/kaestner/repos/qni/trunk/src/ImagingAlgorithms/qt/ImagingAlgorithms-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libImagingAlgorithms.1.0.0.dylib .`
-`$CPCMD /Users/kaestner/repos/src/src/libs/modules/trunk/ModuleConfig/ModuleConfig-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libModuleConfig.1.0.0.dylib .`
-`$CPCMD /Users/kaestner/repos/gui/trunk/qt/QtAddons-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libQtAddons.1.0.0.dylib .`
-#`$CPCMD /Users/kaestner/repos/gui/trunk/qt/QtModuleConfigure-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libQtModuleConfigure.1.0.0.dylib .`
-`$CPCMD /Users/kaestner/repos/src/src/libs/recon2/trunk/ReconFramework/qt/ReconFramework-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libReconFramework.1.0.0.dylib .`
-#`$CPCMD /Users/kaestner/repos/src/src/libs/recon2/trunk/StdBackProjectors/qt/StdBackProjectors-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libStdBackProjectors.1.0.0.dylib .`
-#`$CPCMD /Users/kaestner/repos/src/src/libs/recon2/trunk/StdPreprocModules/qt/StdPreprocModules-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libStdPreprocModules.1.0.0.dylib .`
-`$CPCMD /Users/kaestner/repos/src/src/libs/kipl/trunk/kipl/kipl-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libkipl.1.0.0.dylib .`
-# `$CPCMD /Users/kaestner/QtSDK/Madde/sysroots/harmattan_sysroot_10.2011.34-1_slim/usr/lib/libgomp.so.1.0.0 .`
-#`$CPCMD /Users/kaestner/repos/tomography/trunk/src/StdPreprocModulesGUI-build-desktop-Desktop_Qt_4_8_1_for_GCC__Qt_SDK__Release/libStdPreprocModulesGUI.1.0.0.dylib .`
+`$CPCMD $REPOSPATH/src/libs/modules/trunk/ModuleConfig/ModuleConfig-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libModuleConfig.$EXT .`
+`$CPCMD $REPOSPATH/gui/trunk/qt/QtAddons-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libQtAddons.$EXT .`
+`$CPCMD $REPOSPATH/src/libs/recon2/trunk/ReconFramework/qt/ReconFramework-build-Qt_4_8_1_for_GCC__Qt_SDK__Release/libReconFramework.$EXT .`
+`$CPCMD $REPOSPATH/src/libs/kipl/trunk/kipl/kipl-build_Qt_4_8_1_for_GCC__Qt_SDK__Release/libkipl.$EXT .`
 
-rm -f *.1.dylib
-rm -f *.1.0.dylib
+rm -f *.$EXT1
+rm -f *.$EXT2
 
-for f in `ls *.1.0.0.dylib`; do
-	bn=`basename $f .1.0.0.dylib`
+for f in `ls *.$EXT`; do
+	bn=`basename $f .$EXT`
 	echo $bn
-	ln -s $f $bn.1.0.dylib
-    ln -s $bn.1.0.dylib $bn.1.dylib
+	ln -s $f $bn.$EXT1
+    ln -s $bn.$EXT1 $bn.$EXT2
 done
 
 popd
