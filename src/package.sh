a55c26b48839e2ea76dc1a558ca4bdad060d40b7
kaestner <kaestner@c96eeb0f-4990-4d45-9600-2c6bf17e0709>
2013-11-15 15:58:04 +0000

    Modified default scripts not yet good Added packaging script

---
+++ src/package.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+fname=~/muhrec3_`uname -s`_`uname -m`_`date +%Y%m%d`_rev`svn info |grep Revision | sed -e 's/Revision: //'`.tar.bz2
+
+echo $fname
+pushd .
+cd ~/repos/tomography
+tar -jcvhf $fname muhrec3
+popd 
