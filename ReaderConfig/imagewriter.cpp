406ec56e05fd42368c577cd35a602e54b4cb4026
Anders Kaestner <anderskaestner@users.noreply.github.com>
2015-12-07 07:55:42 +0000

    Activated the write code from comments

--- ReaderConfig/imagewriter.cpp
+++ ReaderConfig/imagewriter.cpp
@@ -1,5 +1,6 @@
 #include <strings/filenames.h>
 #include <io/io_tiff.h>
+#include <io/analyzefileext.h>
 #include "imagewriter.h"
 #include "readerexception.h"
 
@@ -16,23 +17,20 @@ ImageWriter::~ImageWriter()
 
 void ImageWriter::write(kipl::base::TImage<float,2> &img, std::string fname)
 {
-//    kipl::io::eExtensionTypes exttype=kipl::io::GetFileExtensionType(fname);
+    kipl::io::eExtensionTypes exttype=kipl::io::GetFileExtensionType(fname);
 
-//    try {
-//        switch (exttype) {
-//        case kipl::io::ExtensionDMP :
-//        case kipl::io::ExtensionDAT :
-//        case kipl::io::ExtensionRAW : throw ReaderException("Saving raw data is not supported",__FILE__,__LINE__);
-//                break;
-//        case kipl::io::ExtensionFITS: throw ReaderException("Saving image as FITS is not supported",__FILE__,__LINE__);
-//        case kipl::io::ExtensionTIFF: kipl::io::WriTIFF(img,);  break;
-//        case kipl::io::ExtensionTXT : throw ReaderException("Saving image as txt is not supported",__FILE__,__LINE__);
-//        case kipl::io::ExtensionXML : throw ReaderException("Saving image as xml is not supported",__FILE__,__LINE__);
-//        case kipl::io::ExtensionPNG : throw ReaderException("Saving image as png is not supported",__FILE__,__LINE__);
-//        case kipl::io::ExtensionMAT : throw ReaderException("Saving image as mat is not supported",__FILE__,__LINE__);
-//        case kipl::io::ExtensionHDF : throw ReaderException("Saving image as hdf is not supported",__FILE__,__LINE__);
-//        default : throw kipl::base::KiplException("The chosen file type is not implemented",__FILE__,__LINE__);
-//        }
-//    }
-
+    switch (exttype) {
+        case kipl::io::ExtensionDMP :
+        case kipl::io::ExtensionDAT :
+        case kipl::io::ExtensionRAW : throw ReaderException("Saving raw data is not supported",__FILE__,__LINE__);
+                break;
+        case kipl::io::ExtensionFITS: throw ReaderException("Saving image as FITS is not supported",__FILE__,__LINE__);
+        case kipl::io::ExtensionTIFF: kipl::io::WriteTIFF(img,fname.c_str());  break;
+        case kipl::io::ExtensionTXT : throw ReaderException("Saving image as txt is not supported",__FILE__,__LINE__);
+        case kipl::io::ExtensionXML : throw ReaderException("Saving image as xml is not supported",__FILE__,__LINE__);
+        case kipl::io::ExtensionPNG : throw ReaderException("Saving image as png is not supported",__FILE__,__LINE__);
+        case kipl::io::ExtensionMAT : throw ReaderException("Saving image as mat is not supported",__FILE__,__LINE__);
+        case kipl::io::ExtensionHDF : throw ReaderException("Saving image as hdf is not supported",__FILE__,__LINE__);
+        default : throw kipl::base::KiplException("The chosen file type is not implemented",__FILE__,__LINE__);
+    }
 }
