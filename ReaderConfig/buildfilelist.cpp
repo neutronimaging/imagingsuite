040e5db75cd4b6abeb4abb3ac094e4afc34077d8
Anders Kaestner <anderskaestner@users.noreply.github.com>
2016-06-03 15:05:29 +0000

    Added projection file list to the filename builder

--- ReaderConfig/buildfilelist.cpp
+++ ReaderConfig/buildfilelist.cpp
@@ -1,3 +1,5 @@
+#include <map>
+#include <list>
 #include <strings/filenames.h>
 
 #include "buildfilelist.h"
@@ -33,3 +35,40 @@ std::list<std::string> BuildFileList(std::list<ImageLoader> &il, std::list<int> 
     return flist;
 }
 
+std::map<float,std::string> BuildProjectionFileList(std::list<ImageLoader> &il, int sequence, double arc)
+{
+    std::list<int> skiplist;
+
+    return BuildProjectionFileList(il,skiplist,sequence,arc);
+}
+
+std::map<float,std::string> BuildProjectionFileList(std::list<ImageLoader> &il, std::list<int> &skiplist, int sequence, double arc)
+{
+    std::map<float, std::string> flist;
+    std::string fname,ext;
+    const float phi=(1.0f+sqrt(5))*0.5f;
+
+    int fcnt=0;
+    auto skipit=skiplist.begin();
+
+
+    for (auto ilit=il.begin(); ilit!=il.end(); ilit++ ) {
+        ImageLoader &loader = *ilit;
+
+        for (int i=loader.m_nFirst, j=i; i<=loader.m_nLast; i++,fcnt++) {
+            if ((!skiplist.empty()) && (skipit!=skiplist.end())) {
+                if (fcnt==*skipit) {
+                    skipit++;
+                    continue;
+                }
+            }
+            j++;
+            kipl::strings::filenames::MakeFileName(loader.m_sFilemask,i,fname,ext,'#','0');
+            if (sequence==0)
+                flist.insert(make_pair(j,fname));
+            else
+                flist.insert(make_pair(fmod(j*phi*arc,arc),fname));
+        }
+    }
+    return flist;
+}
