3b38d97bb90d912f64621c6e99095d4c6619c3f2
chiaracarminati <chiara.carminati@psi.ch>
2016-11-21 10:07:41 +0000

    standard referencing without BB correction is implemented

--- src/ImagingAlgorithms/include/ReferenceImageCorrection.h
+++ src/ImagingAlgorithms/include/ReferenceImageCorrection.h
@@ -12,8 +12,9 @@
 //#include "stdafx.h"
 #include <base/timage.h>
 #include <logging/logger.h>
-
+#include <string>
 #include "../include/averageimage.h"
+
 
 namespace ImagingAlgorithms {
 
@@ -21,39 +22,48 @@ class IMAGINGALGORITHMSSHARED_EXPORT ReferenceImageCorrection {
 protected:
 	kipl::logging::Logger logger;
 public:
+    enum eReferenceMethod {
+            ReferenceLogNorm,
+            ReferenceBBLogNorm // to be updated with more options, as we decided in the BB meeting
+    };
+
 	ReferenceImageCorrection();
 	virtual ~ReferenceImageCorrection();
 
-    void LoadReferenceImages(std::string obname, size_t firstob, size_t obcnt,
+    void LoadReferenceImages(std::string path, std::string obname, size_t firstob, size_t obcnt,
             std::string dcname, size_t firstdc, size_t dccnt,
             std::string bbname, size_t firstbb, size_t bbcnt,
             size_t *roi,
             size_t *doseroi,
             size_t *bbroi);
 
-	void SetReferenceImages(kipl::base::TImage<float,2> *ob,
-			kipl::base::TImage<float,2> *dc,
-			kipl::base::TImage<float,2> *bb,
-			size_t *doseroi,
-			size_t *bbroi);
+    void SetReferenceImages(kipl::base::TImage<float,2> *ob,
+            kipl::base::TImage<float,2> *dc,
+            kipl::base::TImage<float,2> *bb,
+            float dose_OB,
+            float dose_DC,
+            float dose_BB);
 
-    void SetReferenceImages(kipl::base::TImage<float,3> *ob,
-            kipl::base::TImage<float,3> *dc,
-            kipl::base::TImage<float,3> *bb,
-            size_t *doseroi,
-            size_t *bbroi);
+//    void SetReferenceImages(kipl::base::TImage<float,3> *ob,
+//            kipl::base::TImage<float,3> *dc,
+//            kipl::base::TImage<float,3> *bb,
+//            size_t *doseroi,
+//            size_t *bbroi);
 
 	void ComputeLogartihm(bool x) {m_bComputeLogarithm=x;}
 
-	void Process(kipl::base::TImage<float,2> &img, float dose);
+    kipl::base::TImage<float,2>  Process(kipl::base::TImage<float,2> &img, float dose);
 	void Process(kipl::base::TImage<float,3> &img, float *dose);
+
 
 protected:
 	void PrepareReferences();
 	void PrepareBlackBody(float w=10);
 
-	void ComputeLogNorm(kipl::base::TImage<float,2> &img, float dose);
-	void ComputeNorm(kipl::base::TImage<float,2> &img, float dose);
+    int ComputeLogNorm(kipl::base::TImage<float,2> &img, float dose);
+    void ComputeNorm(kipl::base::TImage<float,2> &img, float dose);
+//    void GetDose(kipl::base::TImage<float,2> &img, size_t *roi);
+
 
 	bool m_bHaveOpenBeam;
 	bool m_bHaveDarkCurrent;
@@ -66,16 +76,24 @@ protected:
 	kipl::base::TImage<float,2> m_BlackBody;
 
 	float m_fOpenBeamDose;
+    float m_fDarkDose;
+    float m_fBlackDose;
 	bool m_bHaveDoseROI;
 	bool m_bHaveBlackBodyROI;
 
     ImagingAlgorithms::AverageImage::eAverageMethod m_AverageMethod;
 
-	size_t m_nDoseROI[4];
-	size_t m_nBlackBodyROI[4];
+    size_t m_nDoseROI[4]; // probably don't need this
+    size_t m_nBlackBodyROI[4]; // neither this
 
 };
 
 }
+
+void IMAGINGALGORITHMSSHARED_EXPORT string2enum(std::string str, ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod &erm);
+
+std::string IMAGINGALGORITHMSSHARED_EXPORT enum2string(ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod &erm);
+
+std::ostream IMAGINGALGORITHMSSHARED_EXPORT & operator<<(ostream & s, ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod erm);
 
 #endif /* REFERENCEIMAGECORRECTION_H_ */
--- src/ImagingAlgorithms/src/ReferenceImageCorrection.cpp
+++ src/ImagingAlgorithms/src/ReferenceImageCorrection.cpp
@@ -7,8 +7,13 @@
 
 //#include "stdafx.h"
 #include "../include/ReferenceImageCorrection.h"
+#include "../include/ImagingException.h"
+#include <strings/miscstring.h>
 
-#include <cstring>
+//#include <cstring>
+#include <map>
+#include <sstream>
+#include <math/median.h>
 
 namespace ImagingAlgorithms {
 
@@ -32,6 +37,7 @@ ReferenceImageCorrection::ReferenceImageCorrection() :
     m_nBlackBodyROI[1]=0;
     m_nBlackBodyROI[2]=0;
     m_nBlackBodyROI[3]=0;
+    std::cout << "ciao image corrector" << std::endl;
 }
 
 ReferenceImageCorrection::~ReferenceImageCorrection()
@@ -39,80 +45,23 @@ ReferenceImageCorrection::~ReferenceImageCorrection()
 
 }
 
-void ReferenceImageCorrection::LoadReferenceImages(std::string obname, size_t firstob, size_t obcnt,
+void ReferenceImageCorrection::LoadReferenceImages(std::string path, std::string obname, size_t firstob, size_t obcnt,
         std::string dcname, size_t firstdc, size_t dccnt,
         std::string bbname, size_t firstbb, size_t bbcnt, size_t *roi,
         size_t *doseroi,
         size_t *bbroi)
 {
-    m_bHaveBlackBody   = false;
-    m_bHaveOpenBeam    = false;
-    m_bHaveDarkCurrent = false;
-    m_bHaveDoseROI = doseroi != nullptr ? true : false;
-    m_bHaveBlackBodyROI = bbroi != nullptr ? true : false;
 
-    ImagingAlgorithms::AverageImage avg;
+    std::cout << "ciao process " << std::endl;
 
-    size_t ndose=max(obcnt,max(dccnt,bbcnt));
-    ndose = 0<ndose ? ndose : 1;
-
-    float *doselist=new float[ndose];
-
-    if ((!obname.empty()) && 0<obcnt) {
-        kipl::base::TImage<float,3> img;
-
-        // Load images
-
-        if (m_bHaveDoseROI) {
-            // load dose
-            m_OpenBeam=avg(img,m_AverageMethod,doselist);
-        }
-        else {
-            m_OpenBeam=avg(img,m_AverageMethod,nullptr);
-        }
-
-        m_bHaveOpenBeam=true;
-    }
-
-    if ((!dcname.empty()) && 0<dccnt) {
-        kipl::base::TImage<float,3> img;
-
-        // Load dark current
-
-        if (m_bHaveDoseROI) {
-            // load dose
-            m_DarkCurrent=avg(img,m_AverageMethod,doselist);
-        }
-        else {
-            m_DarkCurrent=avg(img,m_AverageMethod,nullptr);
-        }
-
-        m_bHaveDarkCurrent=true;
-    }
-
-    if ((!bbname.empty()) && 0<bbcnt) {
-        kipl::base::TImage<float,3> img;
-        // Load bb images
-
-        if (m_bHaveDoseROI) {
-            // load dose
-            m_BlackBody=avg(img,m_AverageMethod,doselist);
-        }
-        else {
-            m_BlackBody=avg(img,m_AverageMethod,nullptr);
-        }
-
-        m_bHaveBlackBody=true;
-    }
-
-    delete [] doselist;
 }
 
 void ReferenceImageCorrection::SetReferenceImages(kipl::base::TImage<float,2> *ob,
-		kipl::base::TImage<float,2> *dc,
-		kipl::base::TImage<float,2> *bb,
-		size_t *doseroi,
-		size_t *bbroi)
+        kipl::base::TImage<float,2> *dc,
+        kipl::base::TImage<float,2> *bb,
+        float dose_OB,
+        float dose_DC,
+        float dose_BB)
 {
 	m_bHaveOpenBeam=m_bHaveDarkCurrent=m_bHaveBlackBody=false;
 	m_bHaveDoseROI=m_bHaveBlackBodyROI=false;
@@ -120,38 +69,54 @@ void ReferenceImageCorrection::SetReferenceImages(kipl::base::TImage<float,2> *o
 	if (ob!=NULL) {
 		m_bHaveOpenBeam=true;
 		m_OpenBeam=*ob;
+        std::cout << "have OB!" << std::endl;
 	}
 
 	if (dc!=NULL) {
 		m_bHaveDarkCurrent=true;
 		m_DarkCurrent=*dc;
+        std::cout << "have DC!" << std::endl;
 	}
 
 	if (bb!=NULL) {
 		m_bHaveBlackBody=true;
 		m_BlackBody=*bb;
+        std::cout << "have BB!" << std::endl;
 	}
 
-	if (doseroi!=NULL) {
+    if (dose_OB!=NULL) {
 		m_bHaveDoseROI=true;
-		memcpy(m_nDoseROI,doseroi,4*sizeof(size_t));
+        m_fOpenBeamDose = dose_OB;
+                std::cout << " have dose roi!  " << std::endl;
+        //        std::cout << dose_OB << std::endl;
+
+
 	}
 
-	if (bbroi!=NULL) {
+    if (dose_DC!=NULL){
+       m_fDarkDose = dose_DC;
+    }
+
+    if (dose_BB!=NULL) {
 		m_bHaveBlackBodyROI=true;
-		memcpy(m_nBlackBodyROI,bbroi,4*sizeof(size_t));
+//		memcpy(m_nBlackBodyROI,dose_BB,4*sizeof(size_t));
 	}
 
 	PrepareReferences();
 	PrepareBlackBody(10.0f);
 }
 
-void ReferenceImageCorrection::Process(kipl::base::TImage<float,2> &img, float dose)
+kipl::base::TImage<float,2> ReferenceImageCorrection::Process(kipl::base::TImage<float,2> &img, float dose)
 {
-	if (m_bComputeLogarithm)
-		ComputeLogNorm(img,dose);
+    if (m_bComputeLogarithm) {
+//        std::cout << "ComputeLogNorm" << std::endl;
+        ComputeLogNorm(img,dose);
+    }
 	else
 		ComputeNorm(img,dose);
+
+    return img;
+
 }
 
 void ReferenceImageCorrection::Process(kipl::base::TImage<float,3> &img, float *dose)
@@ -159,9 +124,9 @@ void ReferenceImageCorrection::Process(kipl::base::TImage<float,3> &img, float *
 	kipl::base::TImage<float, 2> slice(img.Dims());
 
 	for (size_t i=0; i<img.Size(2); i++) {
-		memcpy(slice.GetDataPtr(),img.GetLinePtr(0,i), sizeof(float)*slice.Size());
-
-		Process(slice,dose[i]);
+        memcpy(slice.GetDataPtr(),img.GetLinePtr(0,i), sizeof(float)*slice.Size());
+        Process(slice,dose[i]);
+        memcpy(img.GetLinePtr(0,i), slice.GetDataPtr(), sizeof(float)*slice.Size()); // and copy the result back
 	}
 }
 
@@ -172,17 +137,98 @@ void ReferenceImageCorrection::PrepareBlackBody(float w)
 
 void ReferenceImageCorrection::PrepareReferences()
 {
-	if (m_bComputeLogarithm) {
+    float dose=1.0f/(m_fOpenBeamDose-m_fDarkDose);
+//    if (dose!=dose)
+//        throw ReconException("The reference dose is a NaN",__FILE__,__LINE__);
 
-	}
-	else {
+    const int N=static_cast<int>(m_OpenBeam.Size());
+    float *pFlat=m_OpenBeam.GetDataPtr();
+    float *pDark=m_DarkCurrent.GetDataPtr();
 
-	}
+    if (m_bHaveDarkCurrent) {
+        #pragma omp parallel for
+        for (int i=0; i<N; i++) {
+            float fProjPixel=pFlat[i]-pDark[i];
+            if (fProjPixel<=0)
+                pFlat[i]=0;
+            else
+
+                pFlat[i]=log(fProjPixel*dose);
+
+        }
+    }
+    else {
+        #pragma omp parallel for
+        for (int i=0; i<N; i++) {
+            float fProjPixel=pFlat[i];
+            if (fProjPixel<=0)
+                pFlat[i]=0;
+            else
+               pFlat[i]=log(fProjPixel*dose);
+        }
+    }
+
+// let's assume for now that I do compute the logarithm
+//	if (m_bComputeLogarithm) {
+
+//	}
+//	else {
+
+//	}
+
 }
 
-void ReferenceImageCorrection::ComputeLogNorm(kipl::base::TImage<float,2> &img, float dose)
+int ReferenceImageCorrection::ComputeLogNorm(kipl::base::TImage<float,2> &img, float dose)
 {
+    dose = dose - m_fDarkDose;
+    dose = log(dose<1 ? 1.0f : dose);
 
+    int N=static_cast<int>(img.Size(0)*img.Size(1));
+
+    float *pFlat=m_OpenBeam.GetDataPtr();
+    float *pDark=m_DarkCurrent.GetDataPtr();
+
+//    std::cout << m_bHaveDarkCurrent << " " << m_bHaveOpenBeam << std::endl;
+//    std::cout << "number of pixels: " << N << std::endl;
+
+    if (m_bHaveDarkCurrent) {
+        if (m_bHaveOpenBeam) {
+//                #pragma omp parallel for firstprivate(pFlat,pDark)
+
+                    float *pImg=img.GetDataPtr();
+
+                    #pragma omp parallel for
+                    for (int i=0; i<N; i++) {
+
+//                        if (i==0) std::cout<< "sono dentro" << std::endl; // ci entra..
+
+                        float fProjPixel=(pImg[i]-pDark[i]);
+                        if (fProjPixel<=0)
+                            pImg[i]=0;
+                        else
+                            pImg[i]=pFlat[i]-log(fProjPixel)+dose;
+
+                    }
+
+        }
+        else {
+//            #pragma omp parallel for firstprivate(pDark)
+
+              float *pImg=img.GetDataPtr();
+
+                #pragma omp parallel for
+                for (int i=0; i<N; i++) {
+                    float fProjPixel=(pImg[i]-pDark[i]);
+                    if (fProjPixel<=0)
+                        pImg[i]=0;
+                    else
+                       pImg[i]=-log(fProjPixel)+dose;
+                }
+
+        }
+    }
+
+    return 1;
 }
 
 void ReferenceImageCorrection::ComputeNorm(kipl::base::TImage<float,2> &img, float dose)
@@ -190,4 +236,45 @@ void ReferenceImageCorrection::ComputeNorm(kipl::base::TImage<float,2> &img, flo
 
 }
 
+
+
 }
+
+
+
+
+void string2enum(std::string str, ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod &erm)
+{
+    std::map<std::string, ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod> methods;
+    methods["LogNorm"] = ImagingAlgorithms::ReferenceImageCorrection::ReferenceLogNorm;
+    methods["BBLogNorm"] = ImagingAlgorithms::ReferenceImageCorrection::ReferenceBBLogNorm; // to be updated with more options
+
+    if (methods.count(str)==0)
+        throw ImagingException("The key string does not exist for eReferenceMethod",__FILE__,__LINE__);
+
+    erm=methods[str];
+
+
+}
+
+std::string enum2string(ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod &erm)
+{
+    std::string str;
+
+    switch (erm) {
+        case ImagingAlgorithms::ReferenceImageCorrection::ReferenceLogNorm: str="LogNorm"; break;
+        case ImagingAlgorithms::ReferenceImageCorrection::ReferenceBBLogNorm: str="BBLogNorm"; break;
+        default: throw ImagingException("Unknown reference method in enum2string for eReferenceMethod",__FILE__,__LINE__);
+    }
+    return  str;
+
+}
+
+std::ostream & operator<<(ostream & s, ImagingAlgorithms::ReferenceImageCorrection::eReferenceMethod erm)
+{
+    s<<enum2string(erm);
+
+    return s;
+}
+
+
