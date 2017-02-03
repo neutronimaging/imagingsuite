f4cc03b749132c68933490d3e0fe4a90a49787bd
Anders Kaestner <anderskaestner@users.noreply.github.com>
2017-02-03 18:27:54 +0000

    Fixed a counting bug in the filename list generation

--- ReaderConfig/buildfilelist.cpp
+++ ReaderConfig/buildfilelist.cpp
@@ -1,6 +1,7 @@
 #include <map>
 #include <list>
 #include <strings/filenames.h>
+#include <logging/logger.h>
 
 #include "buildfilelist.h"
 
@@ -44,31 +45,43 @@ std::map<float,std::string> BuildProjectionFileList(std::list<ImageLoader> &il, 
 
 std::map<float,std::string> BuildProjectionFileList(std::list<ImageLoader> &il, std::list<int> &skiplist, int sequence, double arc)
 {
+    kipl::logging::Logger logger("BuildProjectionFileList");
+
+    std::ostringstream msg;
     std::map<float, std::string> flist;
     std::string fname,ext;
     const float phi=(1.0f+sqrt(5))*0.5f;
 
     int fcnt=0;
     auto skipit=skiplist.begin();
+    int j=il.begin()->m_nFirst;
 
-
-    for (auto ilit=il.begin(); ilit!=il.end(); ilit++ ) {
+    for (auto ilit=il.begin(); ilit!=il.end(); ++ilit ) {
         ImageLoader &loader = *ilit;
+        msg.str("");
+        msg<<"Current loader has file mask "<<loader.m_sFilemask<<", first="<<loader.m_nFirst<<", last="<<loader.m_nLast;;
+        logger(logger.LogDebug,msg.str());
 
-        for (int i=loader.m_nFirst, j=i; i<=loader.m_nLast; i++,fcnt++) {
+        for (int i=loader.m_nFirst; i<=loader.m_nLast; ++i,++fcnt) {
             if ((!skiplist.empty()) && (skipit!=skiplist.end())) {
                 if (fcnt==*skipit) {
                     skipit++;
                     continue;
                 }
             }
-            j++;
+
             kipl::strings::filenames::MakeFileName(loader.m_sFilemask,i,fname,ext,'#','0');
             if (sequence==0)
                 flist.insert(make_pair(j,fname));
             else
                 flist.insert(make_pair(fmod(j*phi*arc,arc),fname));
+
+            ++j;
         }
     }
+    msg.str("");
+    msg<<"Built list size="<<flist.size();
+    logger(logger.LogDebug,msg.str());
+
     return flist;
 }
