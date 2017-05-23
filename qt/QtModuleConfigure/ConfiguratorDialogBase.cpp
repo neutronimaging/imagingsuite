a2e63a6e53022a710b5c9fed303dc177a080d471
Anders Kaestner <anderskaestner@users.noreply.github.com>
2017-05-23 16:19:40 +0000

    Added license template

--- qt/QtModuleConfigure/ConfiguratorDialogBase.cpp
+++ qt/QtModuleConfigure/ConfiguratorDialogBase.cpp
@@ -1,14 +1,4 @@
-//
-// This file is part of the ModuleConfigurator library by Anders Kaestner
-// (c) 2010 Anders Kaestner
-// Distribution is only allowed with the permission of the author.
-//
-// Revision information
-// $Author: kaestner $
-// $Date: 2012-05-21 16:24:22 +0200 (Mon, 21 May 2012) $
-// $Rev: 1318 $
-// $Id: ConfiguratorDialogBase.cpp 1318 2012-05-21 14:24:22Z kaestner $
-//
+//<LICENSE>
 
 
 // ModuleConfigurator.cpp : Defines the exported functions for the DLL application.
