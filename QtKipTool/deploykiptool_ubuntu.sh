109c58d43dac5ffefce452a89399d4a4d3ab995c
Anders Kaestner <anderskaestner@users.noreply.github.com>
2017-06-17 13:34:21 +0000

    Adjusted the deployment script

--- QtKipTool/deploykiptool_ubuntu.sh
+++ QtKipTool/deploykiptool_ubuntu.sh
@@ -11,23 +11,26 @@ pushd .
 cd $DEST/Frameworks
 rm -f *.so.1.0.0
 
-`$CPCMD $REPOSPATH/gui/trunk/qt/build-QtAddons-Qt5-Release/libQtAddons.so.1.0.0 .`
-`$CPCMD $REPOSPATH/gui/trunk/qt/build-QtModuleConfigure-Qt5-Release/libQtModuleConfigure.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libQtAddons.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libQtModuleConfigure.so.1.0.0 .`
 
-`$CPCMD $REPOSPATH/qni/trunk/src/ImagingAlgorithms/build-ImagingAlgorithms-Qt5-Release/libImagingAlgorithms.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libImagingAlgorithms.so.1.0.0 .`
 
-`$CPCMD $REPOSPATH/kipl/trunk/kipl/build-kipl-Qt5-Release/libkipl.so.1.0.0 .`
-`$CPCMD $REPOSPATH/modules/trunk/ModuleConfig/build-ModuleConfig-Qt5-Release/libModuleConfig.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/ProcessFramework/build-ProcessFramework-Qt5-Release/libProcessFramework.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libkipl.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libModuleConfig.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libProcessFramework.so.1.0.0 .`
+
+`$CPCMD $REPOSPATH/lib/libReaderConfig.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libReaderGUI.so.1.0.0 .`
 
 # Module libs
-`$CPCMD $REPOSPATH/kiptool/trunk/AdvancedFilterModules/qt/build-AdvancedFilterModules-Qt5-Release/libAdvancedFilterModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/BaseModules/qt/build-BaseModules-Qt5-Release/libBaseModules.so.1.0.0  .`
-`$CPCMD $REPOSPATH/kiptool/trunk/PorespaceModules/qt/build-PorespaceModules-Qt5-Release/libPorespaceModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/ClassificationModules/qt/build-ClassificationModules-Qt5-Release/libClassificationModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/ImagingModules/qt/build-ImagingModules-Qt5-Release/libImagingModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/PCAModules/qt/build-PCAModules-Qt5-Release/libPCAModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/kiptool/trunk/StatisticsModules/qt/build-StatisticsModules-Qt5-Release/libStatisticsModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libAdvancedFilterModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libBaseModules.so.1.0.0  .`
+`$CPCMD $REPOSPATH/lib/libPorespaceModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libClassificationModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libImagingModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libPCAModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/lib/libStatisticsModules.so.1.0.0 .`
 
 
 rm -f *.so
@@ -43,7 +46,7 @@ for f in `ls *.so.1.0.0`; do
 done
 
 cd $DEST/bin
-`$CPCMD $REPOSPATH/kiptool/trunk/QtKipTool/build-QtKipTool-Qt5-Release/QtKipTool .`
+`$CPCMD $REPOSPATH/Applications/QtKipTool .`
 cd ..
 `$CPCMD $REPOSPATH/kiptool/trunk/QtKipTool/scripts/kiptool .`
 chmod +x kiptool
