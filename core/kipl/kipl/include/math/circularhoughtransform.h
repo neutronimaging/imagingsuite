62e011abb8b499a1a700bf32cd71cb1efe759c38
Anders Kaestner <anderskaestner@users.noreply.github.com>
2017-11-03 10:57:58 +0100

    Added derivative option for the transform

--- core/kipl/kipl/include/math/circularhoughtransform.h
+++ core/kipl/kipl/include/math/circularhoughtransform.h
@@ -2,17 +2,21 @@
 #define CIRCULARHOUGHTRANSFORM_H
 
 #include "../base/timage.h"
+#include "../logging/logger.h"
 
 namespace kipl { namespace math {
 
 class CircularHoughTransform
 {
+    kipl::logging::Logger logger;
 public:
     CircularHoughTransform();
 
-    kipl::base::TImage<float,2> operator()(kipl::base::TImage<float> img, float diameter);
+    kipl::base::TImage<float,2> operator()(kipl::base::TImage<float,2> img, float diameter, bool useDerivative=false);
 private:
     void buildKernel(float radius);
+    void absDerivative(bool useDerivative);
+    kipl::base::TImage<float,2> m_Img;
     kipl::base::TImage<float,2> ringKernel;
 };
 }}
--- core/kipl/kipl/src/math/circularhoughtransform.cpp
+++ core/kipl/kipl/src/math/circularhoughtransform.cpp
@@ -1,5 +1,6 @@
 //<LICENSE>
 #include <cmath>
+#include <sstream>
 
 #include "../../include/math/circularhoughtransform.h"
 #include "../../include/drawing/drawing.h"
@@ -8,16 +9,22 @@
 
 namespace kipl { namespace math {
 
-CircularHoughTransform::CircularHoughTransform()
+CircularHoughTransform::CircularHoughTransform() :
+    logger("CircularHoughTransform")
 {
 
 }
 
-kipl::base::TImage<float,2> CircularHoughTransform::operator()(kipl::base::TImage<float,2> img, float radius)
+kipl::base::TImage<float,2> CircularHoughTransform::operator()(kipl::base::TImage<float,2> img, float radius, bool useDerivative)
 {
+    m_Img.Clone(img);
+    absDerivative(useDerivative);
+
+    buildKernel(radius);
+
     kipl::filters::TFilter<float,2> cht(ringKernel.GetDataPtr(),ringKernel.Dims());
 
-    kipl::base::TImage<float,2> chtmap=cht(img,kipl::filters::FilterBase::EdgeMirror);
+    kipl::base::TImage<float,2> chtmap=cht(m_Img,kipl::filters::FilterBase::EdgeMirror);
 
     return chtmap;
 }
@@ -26,7 +33,8 @@ void CircularHoughTransform::buildKernel(float radius)
 {
     int N=ceil(radius+1)*2+1;
     int C=N/2;
-    size_t dims[2]={N,N};
+    size_t dims[2]={static_cast<size_t>(N),static_cast<size_t>(N)};
+
     ringKernel.Resize(dims);
 
     int i=0;
@@ -41,9 +49,31 @@ void CircularHoughTransform::buildKernel(float radius)
         }
     }
 
-    for (int i=0; i<ringKernel.Size(); ++i)
+    for (size_t i=0; i<ringKernel.Size(); ++i)
         ringKernel[i]/=sum;
-
-    return 0;
 }
+
+void CircularHoughTransform::absDerivative(bool useDerivative)
+{
+    if (useDerivative) {
+        logger(logger.LogMessage, "Using absolute derivative");
+
+        size_t filtdims[2]={3,3};
+
+        float vkernel[9]={-3.0f, -10.0f, -3.0f, 0.0f,0.0f,0.0f,3.0f,10.0f,3.0f};
+        kipl::filters::TFilter<float,2> vert(vkernel,filtdims);
+        kipl::base::TImage<float,2> dy=vert(m_Img,kipl::filters::FilterBase::EdgeMirror);
+
+        float hkernel[9]={-3.0f, 0.0f, 3.0f,-10.0f,0.0f,10.0f,-3.0,0.0f,3.0f};
+        kipl::filters::TFilter<float,2> hor(hkernel,filtdims);
+        kipl::base::TImage<float,2> dx=hor(m_Img,kipl::filters::FilterBase::EdgeMirror);
+
+        for (size_t i=0; i<m_Img.Size(); ++i)
+        {
+            m_Img[i]=sqrt(dx[i]*dx[i]+dy[i]*dy[i]);
+        }
+    }
+
+}
+
 }}
