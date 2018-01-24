6c64cf05ec1225aa4373aa1de600f8847f927611
Carminati Maria Chiara <carminati_c@psi.ch>
2018-01-24 11:50:38 +0100

    updated getguimodule and destroyguimodule for unpusblished algorithms. where present, deleted USE_UNPUBLISHED that is no longer used

--- Preprocessing/UnpublPreProcessingGUI/unpublpreprocessinggui.cpp
+++ Preprocessing/UnpublPreProcessingGUI/unpublpreprocessinggui.cpp
@@ -1,6 +1,36 @@
 #include "unpublpreprocessinggui.h"
+#include "robustlognormdlg.h"
 
+class ConfiguratorDialogBase;
 
-UnpublPreProcessingGUI::UnpublPreProcessingGUI()
+//UnpublPreProcessingGUI::UnpublPreProcessingGUI()
+//{
+//}
+
+UNPUBLPREPROCESSINGGUISHARED_EXPORT void * GetGUIModule(const char *application, const char *name, void *interactor)
 {
+    if (strcmp(application,"muhrec")!=0)
+        return nullptr;
+
+    if (name!=nullptr) {
+        std::string sName=name;
+
+        if (sName=="RobustLogNorm")
+            return new RobustLogNormDlg;
+    }
+
+
+    return NULL;
+}
+
+UNPUBLPREPROCESSINGGUISHARED_EXPORT int DestroyGUIModule(const char *application, void *obj)
+{
+     if (strcmp(application,"muhrec")!=0)
+         return -1;
+
+     if (obj!=nullptr) {
+         ConfiguratorDialogBase *dlg=reinterpret_cast<ConfiguratorDialogBase *>(obj);
+         delete dlg;
+     }
+    return 0;
 }
--- Preprocessing/UnpublPreProcessingGUI/unpublpreprocessinggui.h
+++ Preprocessing/UnpublPreProcessingGUI/unpublpreprocessinggui.h
@@ -3,11 +3,21 @@
 
 #include "unpublpreprocessinggui_global.h"
 
-class UNPUBLPREPROCESSINGGUISHARED_EXPORT UnpublPreProcessingGUI
-{
+//class UNPUBLPREPROCESSINGGUISHARED_EXPORT UnpublPreProcessingGUI
+//{
 
-public:
-    UnpublPreProcessingGUI();
-};
+//public:
+//    UnpublPreProcessingGUI();
+//    void *GetGUIModule(const char *application, const char *name,void *interactor);
+//    int  DestroyGUIModule(const char *application, void *obj);
+
+//private:
+
+//};
+
+extern "C" {
+void UNPUBLPREPROCESSINGGUISHARED_EXPORT * GetGUIModule(const char *application, const char *name,void *interactor);
+int UNPUBLPREPROCESSINGGUISHARED_EXPORT DestroyGUIModule(const char *application, void *obj);
+}
 
 #endif // UNPUBLPREPROCESSINGGUI_H
