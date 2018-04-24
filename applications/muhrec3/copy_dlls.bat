c1d0f94af119f255edcc1d429e63fd5babcf9dd8
carminati_c <chiara.carminati@psi.ch>
2018-04-24 15:11:58 +0200

    updated deployment bat script for windows
      (use "git checkout -- <file>..." to discard changes in working directory)

--- applications/muhrec3/copy_dlls.bat
+++ applications/muhrec3/copy_dlls.bat
@@ -9,18 +9,24 @@ pushd .
 cd %DEST%
 mkdir resources
 
-copy C:\%HOMEPATH%\lib\ImagingAlgorithms.dll .
-copy C:\%HOMEPATH%\lib\ModuleConfig.dll .
-copy C:\%HOMEPATH%\lib\QtAddons.dll .
-copy C:\%HOMEPATH%\lib\QtModuleConfigure.dll .
-copy C:\%HOMEPATH%\lib\kipl.dll .
-copy C:\%HOMEPATH%\lib\ReconFramework.dll .
-copy C:\%HOMEPATH%\lib\StdBackProjectors.dll .
-copy C:\%HOMEPATH%\lib\StdPreprocModules.dll .
-copy C:\%HOMEPATH%\lib\StdPreprocModulesGUI.dll .
-copy C:\%HOMEPATH%\lib\InspectorModulesGUI.dll .
-copy C:\%HOMEPATH%\lib\InspectorModules.dll .
-copy C:\%HOMEPATH%\lib\FDKBackProjectors.dll .
+copy %REPOS%\lib\ImagingAlgorithms.dll .
+copy %REPOS%\lib\ModuleConfig.dll .
+copy %REPOS%\lib\QtAddons.dll .
+copy %REPOS%\lib\QtModuleConfigure.dll .
+copy %REPOS%\lib\kipl.dll .
+copy %REPOS%\lib\ReconFramework.dll .
+copy %REPOS%\lib\StdBackProjectors.dll .
+copy %REPOS%\lib\StdPreprocModules.dll .
+copy %REPOS%\lib\StdPreprocModulesGUI.dll .
+copy %REPOS%\lib\InspectorModulesGUI.dll .
+copy %REPOS%\lib\InspectorModules.dll .
+copy %REPOS%\lib\FDKBackProjectors.dll .
+copy %REPOS%\lib\UnpublishedImagingAlgorithms.dll .
+copy %REPOS%\lib\UnpublPreProcessing.dll .
+copy %REPOS%\lib\UnpublPreProcessingGUI.dll .
+#copy %REPOS%\lib\ReaderConfig.dll .
+#copy %REPOS%\lib\ReaderGUI.dll .
+#copy %REPOS%\lib\ReconAlgorithms.dll .
 
 copy %REPOS%\imagingsuite\external\lib64\libtiff.dll .
 copy %REPOS%\imagingsuite\external\lib64\libjpeg-62.dll .
@@ -37,15 +43,16 @@ copy %REPOS%\imagingsuite\external\lib64\hdf5\hdf5_cpp.dll .
 copy %REPOS%\imagingsuite\external\lib64\hdf5\zlib.dll .
 copy %REPOS%\imagingsuite\external\lib64\hdf5\szip.dll .
 
-
-copy C:\%HOMEPATH%\Applications\muhrec3.exe .
-
+copy %REPOS%\Applications\muhrec3.exe .
 
 copy %REPOS%\imagingsuite\applications\muhrec3\src\resources resources
 
-rem cd C:\Qt\5.8\msvc2015_64\bin
-rem cd C:\Qt\5.9.3\msvc2015_64\bin
-cd C:\Qt\Qt5.9.3\5.9.3\msvc2015_64\bin
+#rem cd C:\Qt\5.8\msvc2015_64\bin
+#rem cd C:\Qt\5.9.3\msvc2015_64\bin
+#cd C:\Qt\Qt5.9.3\5.9.3\msvc2015_64\bin
+
+cd C:\Qt\5.9\msvc2015_64\bin
+
 windeployqt %DEST%\muhrec3.exe
 copy Qt5PrintSupport.dll %DEST%
 
