dec1acde5b47735b88a487f4ca88f92170432154
Anders Kaestner <anders.kaestner@psi.ch>
2018-06-15 14:49:52 +0200

    Reformatted and updated standard data types. Next step TNT.

--- core/kipl/kipl/src/math/nonlinfit.cpp
+++ core/kipl/kipl/src/math/nonlinfit.cpp
@@ -23,140 +23,172 @@ struct Fitmrq {
 //vectors and fitting functions and to input an initial parameter guess. Then call any combination
 //of hold, free, and fit as often as desired. fit sets the output quantities a, covar, alpha,
 //and chisq.
-static const int NDONE=4, ITMAX=1000; //Convergence parameters.
-Int ndat, ma, mfit;
-VecDoub_I &x,&y,&sig;
-Doub tol;
-void (*funcs)(const Doub, VecDoub_I &, Doub &, VecDoub_O &);
-VecBool ia;
-VecDoub a; //Output values. a is the vector of fitted coefficients, covar is its covariance matrix, alpha is the curvature matrix, and chisq is the value of 2 fothe fit.
-MatDoub covar;
-MatDoub alpha;
-double chisq;
-Fitmrq(VecDoub_I &xx, VecDoub_I &yy, VecDoub_I &ssig, VecDoub_I &aa,
-void funks(const Doub, VecDoub_I &, Doub &, VecDoub_O &), const Doub
-TOL=1.e-3) : ndat(xx.size()), ma(aa.size()), x(xx), y(yy), sig(ssig),
-tol(TOL), funcs(funks), ia(ma), alpha(ma,ma), a(aa), covar(ma,ma) {
-//Constructor. Binds references to the data arrays xx, yy, and ssig, and to a user-supplied
-//function funks that calculates the nonlinear fitting function and its derivatives. Also inputs
-//the initial parameters guess aa (which is copied, not modified) and an optional convergence
-//tolerance TOL. Initializes all parameters as free (not held).
-    for (int i=0;i<ma;i++) ia[i] = true;
-}
-void hold(const Int i, const Doub val) {ia[i]=false; a[i]=val;}
-void free(const Int i) {ia[i]=true;}
-//Optional functions for holding a parameter, identified by a value i in the range 0; : : : ;ma-1,
-//fixed at the value val, or for freeing a parameter that was previously held fixed. hold and
-//free may be called for any number of parameters before calling fit to calculate best-fit
-//values for the remaining (not held) parameters, and the process may be repeated multiple
-//times.
-void fit() {
-//    Iterate to reduce the 2 of a fit between a set of data points x[0..ndat-1], y[0..ndat-1]
-//    with individual standard deviations sig[0..ndat-1], and a nonlinear function that depends
-//    on ma coefficients a[0..ma-1]. When 2 is no longer decreasing, set best-fit values
-//    for the parameters a[0..ma-1], and chisq D 2, covar[0..ma-1][0..ma-1], and
-//    alpha[0..ma-1][0..ma-1]. (Parameters held fixed will return zero covariances.)
-    Int j,k,l,iter,done=0;
-    Doub alamda=.001,ochisq;
-    VecDoub atry(ma),beta(ma),da(ma);
-    mfit=0;
-    for (j=0;j<ma;j++)
-        if (ia[j]) mfit++;
-    MatDoub oneda(mfit,1), temp(mfit,mfit);
-    mrqcof(a,alpha,beta); // Initialization.
-    for (j=0;j<ma;j++)
-        atry[j]=a[j];
-    ochisq=chisq;
+    static const int NDONE=4, ITMAX=1000; //Convergence parameters.
+    int ndat, ma, mfit;
+    VecDoub_I &x,&y,&sig;
+    double tol;
+    void (*funcs)(const Doub, VecDoub_I &, Doub &, VecDoub_O &);
+    VecBool ia;
+    VecDoub a; //Output values. a is the vector of fitted coefficients, covar is its covariance matrix, alpha is the curvature matrix, and chisq is the value of 2 fothe fit.
+    MatDoub covar;
+    MatDoub alpha;
+    double chisq;
 
-    for (iter=0;iter<ITMAX;iter++) {
-        if (done==NDONE) alamda=0.; //Last pass. Use zero alamda.
-        for (j=0;j<mfit;j++) { //Alter linearized fitting matrix, by augmenting diagonal elements.
-            for (k=0;k<mfit;k++) covar[j][k]=alpha[j][k];
-        covar[j][j]=alpha[j][j]*(1.0+alamda);
-        for (k=0;k<mfit;k++)
-            temp[j][k]=covar[j][k];
-        oneda[j][0]=beta[j];
+    void Fitmrq(VecDoub_I &xx, VecDoub_I &yy, VecDoub_I &ssig,
+                VecDoub_I &aa,
+                void funks(const Doub, VecDoub_I &, Doub &, VecDoub_O &),
+                const double TOL=1.e-3) :
+        ndat(xx.size()),
+        ma(aa.size()),
+        x(xx),
+        y(yy),
+        sig(ssig),
+        tol(TOL),
+        funcs(funks),
+        ia(ma),
+        alpha(ma,ma),
+        a(aa),
+        covar(ma,ma)
+    {
+        // Constructor.
+        // Binds references to the data arrays xx, yy, and ssig, and to a user-supplied
+        // function funks that calculates the nonlinear fitting function and its derivatives. Also inputs
+        // the initial parameters guess aa (which is copied, not modified) and an optional convergence
+        // tolerance TOL. Initializes all parameters as free (not held).
+        for (int i=0;i<ma;i++)
+            ia[i] = true;
+     }
+
+    void hold(const int i, const doub val)
+    {
+        ia[i]=false;
+        a[i]=val;
     }
-    gaussj(temp,oneda); //Matrix solution.
 
-    for (j=0;j<mfit;j++) {
-        for (k=0;k<mfit;k++) covar[j][k]=temp[j][k];
-            da[j]=oneda[j][0];
+    void free(const Int i)
+    {
+        ia[i]=true;
     }
-    if (done==NDONE) { //Converged. Clean up and return.
-        covsrt(covar);
-        covsrt(alpha);
-        return;
-    }
-    for (j=0,l=0;l<ma;l++) //Did the trial succeed?
-        if (ia[l]) atry[l]=a[l]+da[j++];
 
-    mrqcof(atry,covar,da);
+    void fit()
+    {
+        //    Iterate to reduce the 2 of a fit between a set of data points x[0..ndat-1], y[0..ndat-1]
+        //    with individual standard deviations sig[0..ndat-1], and a nonlinear function that depends
+        //    on ma coefficients a[0..ma-1]. When 2 is no longer decreasing, set best-fit values
+        //    for the parameters a[0..ma-1], and chisq D 2, covar[0..ma-1][0..ma-1], and
+        //    alpha[0..ma-1][0..ma-1]. (Parameters held fixed will return zero covariances.)
 
-    if (abs(chisq-ochisq) < MAX(tol,tol*chisq)) done++;
+        int j,k,l,iter,done=0;
+        double alamda=.001,ochisq;
+        VecDoub atry(ma),beta(ma),da(ma);
+        mfit=0;
+        for ( j=0; j<ma; j++)
+            if (ia[j]) mfit++;
 
-    if (chisq < ochisq) { //Success, accept the new solution.
-        alamda *= 0.1;
+        MatDoub oneda(mfit,1), temp(mfit,mfit);
+
+        mrqcof(a,alpha,beta); // Initialization.
+
+        for (j=0;j<ma;j++)
+            atry[j]=a[j];
+
         ochisq=chisq;
-        for (j=0;j<mfit;j++) {
-            for (k=0;k<mfit;k++) alpha[j][k]=covar[j][k];
-            beta[j]=da[j];
-        }
-        for (l=0;l<ma;l++)
-            a[l]=atry[l];
-    }
-    else { //Failure, increase alamda.
-        alamda *= 10.0;
-        chisq=ochisq;
-    }
-    }
-    throw("Fitmrq too many iterations");
-    }
 
-void mrqcof(VecDoub_I &a, MatDoub_O &alpha, VecDoub_O &beta) {
-//Used by fit to evaluate the linearized fitting matrix alpha, and vector beta as in (15.5.8), and to calculate 2.
-    Int i,j,k,l,m;
-    Doub ymod,wt,sig2i,dy;
-    VecDoub dyda(ma);
-    for (j=0;j<mfit;j++) { //Initialize (symmetric) alpha, beta.
-        for (k=0;k<=j;k++) alpha[j][k]=0.0;
-            beta[j]=0.;
-    }
+        for (iter=0;iter<ITMAX;iter++) {
+            if (done==NDONE) alamda=0.; //Last pass. Use zero alamda.
+                for (j=0;j<mfit;j++) { //Alter linearized fitting matrix, by augmenting diagonal elements.
+                    for (k=0;k<mfit;k++) covar[j][k]=alpha[j][k];
+                covar[j][j]=alpha[j][j]*(1.0+alamda);
+                for (k=0;k<mfit;k++)
+                    temp[j][k]=covar[j][k];
+                oneda[j][0]=beta[j];
+            }
 
-    chisq=0.;
-    for (i=0;i<ndat;i++) { //Summation loop over all data.
-        funcs(x[i],a,ymod,dyda);
-        sig2i=1.0/(sig[i]*sig[i]);
-        dy=y[i]-ymod;
-        for (j=0,l=0;l<ma;l++) {
-            if (ia[l]) {
-                wt=dyda[l]*sig2i;
-                for (k=0,m=0;m<l+1;m++)
-                    if (ia[m]) alpha[j][k++] += wt*dyda[m];
-                beta[j++] += dy*wt;
+            gaussj(temp,oneda); //Matrix solution.
+
+            for (j=0;j<mfit;j++) {
+                for (k=0;k<mfit;k++) covar[j][k]=temp[j][k];
+                    da[j]=oneda[j][0];
+            }
+
+            if (done==NDONE) { //Converged. Clean up and return.
+                covsrt(covar);
+                covsrt(alpha);
+                return;
+            }
+
+            for (j=0,l=0;l<ma;l++) //Did the trial succeed?
+                if (ia[l]) atry[l]=a[l]+da[j++];
+
+            mrqcof(atry,covar,da);
+
+            if (abs(chisq-ochisq) < MAX(tol,tol*chisq)) done++;
+
+            if (chisq < ochisq) { //Success, accept the new solution.
+                alamda *= 0.1;
+                ochisq=chisq;
+                for (j=0;j<mfit;j++) {
+                    for (k=0;k<mfit;k++) alpha[j][k]=covar[j][k];
+                    beta[j]=da[j];
+                }
+                for (l=0;l<ma;l++)
+                    a[l]=atry[l];
+            }
+            else { //Failure, increase alamda.
+                alamda *= 10.0;
+                chisq=ochisq;
             }
         }
-        chisq += dy*dy*sig2i; // And find 2.
+        throw("Fitmrq too many iterations");
     }
-    for (j=1;j<mfit;j++) //Fill in the symmetric side.
-        for (k=0;k<j;k++) alpha[k][j]=alpha[j][k];
-}
 
-void covsrt(MatDoub_IO &covar) {
-//Expand in storage the covariance matrix covar, so as to take into account parameters that
-//are being held fixed. (For the latter, return zero covariances.)
-    Int i,j,k;
-    for (i=mfit;i<ma;i++)
-    for (j=0;j<i+1;j++) covar[i][j]=covar[j][i]=0.0;
-    k=mfit-1;
-    for (j=ma-1;j>=0;j--) {
-        if (ia[j]) {
-            for (i=0;i<ma;i++) SWAP(covar[i][k],covar[i][j]);
-            for (i=0;i<ma;i++) SWAP(covar[k][i],covar[j][i]);
-            k--;
+    void mrqcof(VecDoub_I &a, MatDoub_O &alpha, VecDoub_O &beta)
+    {
+    //Used by fit to evaluate the linearized fitting matrix alpha, and vector beta as in (15.5.8), and to calculate 2.
+        int i,j,k,l,m;
+        double ymod,wt,sig2i,dy;
+        VecDoub dyda(ma);
+        for (j=0; j<mfit; j++) { //Initialize (symmetric) alpha, beta.
+            for (k=0; k<=j; k++) alpha[j][k]=0.0;
+                beta[j]=0.;
+        }
+
+        chisq=0.;
+
+        for (i=0; i<ndat; i++) { //Summation loop over all data.
+            funcs(x[i],a,ymod,dyda);
+            sig2i=1.0/(sig[i]*sig[i]);
+            dy=y[i]-ymod;
+            for (j=0,l=0;l<ma;l++) {
+                if (ia[l]) {
+                    wt=dyda[l]*sig2i;
+                    for (k=0,m=0;m<l+1;m++)
+                        if (ia[m]) alpha[j][k++] += wt*dyda[m];
+                    beta[j++] += dy*wt;
+                }
+            }
+            chisq += dy*dy*sig2i; // And find 2.
+        }
+        for (j=1;j<mfit;j++) //Fill in the symmetric side.
+            for (k=0;k<j;k++) alpha[k][j]=alpha[j][k];
+    }
+
+    void covsrt(MatDoub_IO &covar)
+    {
+    //Expand in storage the covariance matrix covar, so as to take into account parameters that
+    //are being held fixed. (For the latter, return zero covariances.)
+        int i,j,k;
+        for (i=mfit;i<ma;i++)
+        for (j=0;j<i+1;j++) covar[i][j]=covar[j][i]=0.0;
+        k=mfit-1;
+        for (j=ma-1;j>=0;j--) {
+            if (ia[j]) {
+                for (i=0;i<ma;i++) SWAP(covar[i][k],covar[i][j]);
+                for (i=0;i<ma;i++) SWAP(covar[k][i],covar[j][i]);
+                k--;
+            }
         }
     }
-}
 };
 
 }
