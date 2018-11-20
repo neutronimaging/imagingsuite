6a1036cc9c56f90ffedcb2ece31dba1b2bbee1e5
Anders Kaestner <anders.kaestner@psi.ch>
2018-11-20 15:17:18 +0100

    Checked if fix 200 works on mac. Is OK.
    Move dark style settings to QtAddons for using on other applications.

---
+++ GUI/qt/QtAddons/facestyles.h
@@ -0,0 +1,14 @@
+//<LICENSE>
+
+#ifndef FACESTYLES_H
+#define FACESTYLES_H
+
+#include "QtAddons_global.h"
+
+class QApplication;
+
+namespace QtAddons {
+
+void QTADDONSSHARED_EXPORT setDarkFace(QApplication *app);
+}
+#endif // FACESTYLES_H
