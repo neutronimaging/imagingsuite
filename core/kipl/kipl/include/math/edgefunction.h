624fc95c45ad890bde141a40f00904381d365fe3
chiaracarminati <chiara.carminati@psi.ch>
2019-04-12 17:28:55 +0200

    implementation of more advanced edge formulation with linear fit before and after the edge. output to be tested

--- core/kipl/kipl/include/math/edgefunction.h
+++ core/kipl/kipl/include/math/edgefunction.h
@@ -20,19 +20,28 @@ public:
     edgefunction();
     ~edgefunction();
 
-//private:
+//private: // should those be private or not?
+    double *term1;
+    double *term2;
     double *term3;
-    double *term5;
     double *term4;
+    double *term5;
     double *edgeFunction;
     double sigma;
     double alpha;
     double t0;
+    double a1;
+    double a2;
+    double a5;
+    double a6;
     double *t;
     int size;
     void computeTerm4(double *fun, double *t);
     void computeTerm3(double *fun, double *t);
     void computeTerm5(double *fun, double *t);
+    void computeTerm1(double *fun, double *t);
+    void computeTerm2(double *fun, double *t);
+    void computeSimplifiedEdge(double *t);
     void computeEdge(double *t);
 };
 
--- core/kipl/kipl/src/math/edgefunction.cpp
+++ core/kipl/kipl/src/math/edgefunction.cpp
@@ -7,6 +7,8 @@ namespace kipl {namespace math {
 
 
 edgefunction::edgefunction() :
+    term1(nullptr),
+    term2(nullptr),
     term3(nullptr),
     term4(nullptr),
     term5(nullptr),
@@ -15,10 +17,16 @@ edgefunction::edgefunction() :
     alpha(-0.1),
     t0(0.0),
     t(nullptr),
-    size(200)
+    size(200),
+    a1(0),
+    a2(0),
+    a5(0),
+    a6(0)
 {
         if(t!=nullptr){
             size = static_cast<int>(sizeof(t)/sizeof(*t));
+            term1 = new double[size];
+            term2 = new double[size];
             term3 = new double[size];
             term4 = new double[size];
             term5 = new double[size];
@@ -28,6 +36,15 @@ edgefunction::edgefunction() :
 
 edgefunction::~edgefunction()
 {
+    if(term1!=nullptr){
+        delete[] term1;
+        term1 = nullptr;
+    }
+
+    if(term2!=nullptr){
+        delete[] term2;
+        term2 = nullptr;
+    }
 
     if(term3!=nullptr){
         delete[] term3;
@@ -44,6 +61,7 @@ edgefunction::~edgefunction()
         term5 = nullptr;
     }
 
+
     if(edgeFunction!=nullptr){
         delete[] edgeFunction;
         edgeFunction = nullptr;
@@ -52,6 +70,24 @@ edgefunction::~edgefunction()
     if(t!=nullptr){
         delete[] t;
         t = nullptr;
+    }
+
+}
+
+void edgefunction::computeTerm1(double *fun, double *t)
+{
+
+    for (int i=0; i<size; ++i)
+    {
+        fun[i] = a1+a2*(t[i]-a6);
+    }
+
+}
+
+void edgefunction::computeTerm2(double *fun, double *t)
+{
+    for (int i=0; i<size;++i) {
+        fun[i] = ((a5-a2)/2)*(t[i]-a6);
     }
 
 }
@@ -86,7 +122,7 @@ void edgefunction::computeTerm4(double *fun, double *t){
 
 }
 
-void edgefunction::computeEdge(double *t)
+void edgefunction::computeSimplifiedEdge(double *t)
 {
 
     computeTerm4(term4,t);
@@ -101,5 +137,21 @@ void edgefunction::computeEdge(double *t)
 
 }
 
+void edgefunction::computeEdge(double *t)
+{
+    computeTerm1(term1,t);
+    computeTerm2(term2,t);
+    computeTerm3(term3,t);
+    computeTerm4(term4,t);
+    computeTerm5(term5,t);
+
+    for (int i=0; i<size; ++i)
+    {
+        edgeFunction[i] = term1[i]+term2[i]*(1-(term3[i]-term4[i]*term5[i]));
+    }
+
+
+
+}
 
 }}
