98b97cca7b5d68155916f23d32acfbc9167affb9
Anders Kaestner <anders.kaestner@psi.ch>
2019-06-20 13:53:48 +0200

    Added an update plot button when the number of skipped exceed ten projection.

--- applications/muhrec/src/findskiplistdialog.h
+++ applications/muhrec/src/findskiplistdialog.h
@@ -16,7 +16,7 @@ class FindSkipListDialog : public QDialog
     Q_OBJECT
     kipl::logging::Logger logger;
 public:
-    explicit FindSkipListDialog(QWidget *parent = 0);
+    explicit FindSkipListDialog(QWidget *parent = nullptr);
     ~FindSkipListDialog();
     
     int exec(ReconConfig &config);
@@ -26,14 +26,18 @@ protected:
     virtual int exec() { return QDialog::exec();}
     void LoadDoseList(ReconConfig &config);
 
-protected slots:
-    void ChangedNumberOfProjections(int x);
+private slots:
+    void on_pushButton_updatePlot_clicked();
+
+    void on_spinExpNumProj_valueChanged(int arg1);
+
 private:
     Ui::FindSkipListDialog *ui;
 
     int m_nMaxNumberProjections;
     QVector<QPointF> m_DoseData;
     std::multimap<float,int > m_SortedDoses;
+    std::set<int> m_sortedSkip;
 };
 
 #endif // FINDSKIPLISTDIALOG_H
