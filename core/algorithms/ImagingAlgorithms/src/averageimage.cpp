6b204a8f2e9e35d82a396b8959c8dec28d3feeed
Anders Kaestner <anders.kaestner@psi.ch>
2019-07-03 09:44:06 +0200

    Moved binder code to class source file to distribute compilation.

--- core/algorithms/ImagingAlgorithms/src/averageimage.cpp
+++ core/algorithms/ImagingAlgorithms/src/averageimage.cpp
@@ -259,3 +259,55 @@ std::ostream & operator<<(ostream & s, ImagingAlgorithms::AverageImage::eAverage
 
     return s;
 }
+
+
+#ifdef HAVEPYBIND11
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+void bindAverageImage(py::module &m)
+{
+    py::class_<ImagingAlgorithms::AverageImage> avgClass(m, "AverageImage");
+    avgClass.def(py::init());
+    avgClass.def("windowSize", &ImagingAlgorithms::AverageImage::windowSize,"Returns the size of the filter window used by the weighted average");
+    avgClass.def("setWindowSize", &ImagingAlgorithms::AverageImage::setWindowSize,"Set the size of the filter window used by the weighted average",py::arg("size")=5);
+    avgClass.def("process",
+                 [](ImagingAlgorithms::AverageImage &a,
+                 py::array_t<float> &x,
+                 ImagingAlgorithms::AverageImage::eAverageMethod method,
+                 std::vector<float> weights = {})
+    {
+        auto r = x.unchecked<3>(); // x must have ndim = 3; can be non-writeable
+
+        py::buffer_info buf1 = x.request();
+
+        size_t dims[]={static_cast<size_t>(buf1.shape[2]),
+                       static_cast<size_t>(buf1.shape[1]),
+                       static_cast<size_t>(buf1.shape[0])};
+        kipl::base::TImage<float,3> stack(static_cast<float*>(buf1.ptr),dims);
+
+        kipl::base::TImage<float,2> res=a(stack,method,weights);
+
+        py::array_t<float> avg = py::array_t<float>(res.Size());
+        avg.resize({res.Size(1),res.Size(0)});
+        kipl::base::TImage<float,2> avgimg(static_cast<float*>(avg.request().ptr),dims);
+        std::copy_n(res.GetDataPtr(),res.Size(),avgimg.GetDataPtr());
+        return avg;
+    },"Computes a combined image using the selected method",py::arg("img"),py::arg("method"),py::arg("weights")=std::vector<float>());
+
+
+    py::enum_<ImagingAlgorithms::AverageImage::eAverageMethod>(avgClass,"eAverageMethod")
+            .value("ImageSum",             ImagingAlgorithms::AverageImage::ImageSum)
+            .value("ImageAverage",         ImagingAlgorithms::AverageImage::ImageAverage)
+            .value("ImageMedian",          ImagingAlgorithms::AverageImage::ImageMedian)
+            .value("ImageWeightedAverage", ImagingAlgorithms::AverageImage::ImageWeightedAverage)
+            .value("ImageMin",             ImagingAlgorithms::AverageImage::ImageMin)
+            .value("ImageMax",             ImagingAlgorithms::AverageImage::ImageMax)
+            .export_values();
+
+}
+
+#endif
--- core/algorithms/ImagingAlgorithms/src/pybinder.cpp
+++ core/algorithms/ImagingAlgorithms/src/pybinder.cpp
@@ -1,1 +1,23 @@
 #include "../include/pybinder.h"
+
+#ifdef HAVEPYBIND11
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+#include "../include/ImagingException.h"
+
+namespace py = pybind11;
+
+void bindAverageImage(py::module &m);
+void bindPolynomialCorrection(py::module &m);
+
+PYBIND11_MODULE(imagalg,m)
+{
+    bindAverageImage(m);
+    bindPolynomialCorrection(m);
+
+    py::register_exception<ImagingException>(m, "ImagingException");
+}
+
+#endif
