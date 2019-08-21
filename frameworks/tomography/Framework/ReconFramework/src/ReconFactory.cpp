f5ec5f947fd31c72296a91ae04a0323b87abd6b1
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2019-08-21 17:06:06 +0200

    Groudwork for making engine. TODO pass interpreter

--- frameworks/tomography/Framework/ReconFramework/src/ReconFactory.cpp
+++ frameworks/tomography/Framework/ReconFramework/src/ReconFactory.cpp
@@ -5,6 +5,7 @@
 #include "../include/PreprocModuleBase.h"
 #include "../include/ReconException.h"
 #include <iostream>
+#include <memory>
 
 ReconFactory::ReconFactory(void) :
 	logger("ReconFactory")
@@ -77,18 +78,91 @@ void ReconFactory::SetBackProjector(ReconConfig &config, ReconEngine * engine, k
     }
 }
 
-#ifdef HAVEPYBIND11
+// #ifdef HAVEPYBIND11
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+void fillUserInformation(ReconConfig& config, py::dict& userInformation)
+{
+    for (auto item : userInformation) {
+        const auto key = item.first.cast<std::string>();
+        const auto value = item.second.cast<std::string>();
+        if (key == "operator") {
+            config.UserInformation.sOperator = value;
+        } else if (key == "instrument") {
+            config.UserInformation.sInstrument = value;
+        } else if (key == "project_number") {
+            config.UserInformation.sProjectNumber = value;
+        } else if (key == "sample") {
+            config.UserInformation.sSample = value;
+        } else if (key == "comment") {
+            config.UserInformation.sComment = value;
+        } else if (key == "date") {
+            config.UserInformation.sDate = value;
+        } else if (key == "version") {
+            config.UserInformation.sVersion = value;
+        } else {
+            throw std::runtime_error("Unknown key " + key + "for UserInformation");
+        }
+        std::cout << "UserInformation -- key: " << item.first << ", value: " << item.second << '\n';
+    }
+}
+void fillSystem(ReconConfig& config, py::dict& system)
+{
+    std::cout << "TODO for system\n";
+}
+void fillProjections(ReconConfig& config, py::dict& projections)
+{
+    std::cout << "TODO for proj\n";
+}
+void fillMatrix(ReconConfig& config, py::dict& matrix)
+{
+    std::cout << "TODO for matrix\n";
+}
+void makeEngineFromDict(py::dict userInformation, py::dict system, py::dict projections, py::dict matrix) // , py::dict proj, py::dict matrix, py::dict processchain
+{
+    std::cout << "got called with\n";
+    ReconConfig config{};
+    fillUserInformation(config, userInformation);
+    fillSystem(config, system);
+    fillProjections(config, projections);
+    fillMatrix(config, matrix);
+    // fillModuleConfig(config, ...);
+}
 
+std::unique_ptr<ReconEngine> makeEngineFromXML(const std::string xmlFile, const std::string projectName)
+{
+    std::cout << "Loading config file '" << xmlFile << "' with project name '" << projectName << "'\n";
+    ReconConfig config{};
+    try {
+        config.LoadConfigFile(xmlFile, projectName);
+        std::cout << "Loaded with information uhh: " << config.UserInformation.sOperator << '\n';
+    } catch (kipl::base::KiplException& a) {
+        std::cout << "Encountered exception: " << a.what() << '\n';
+    }
+
+    ReconFactory factory{};
+    return std::unique_ptr<ReconEngine>(factory.BuildEngine(config));
+}
 void bindReconFactory(py::module &m)
 {
-    py::class_<ReconFactory> rfClass(m, "ReconFactory");
+    // py::class_<ReconFactory> rfClass(m, "ReconFactory");
 
-    rfClass.def(py::init());
+    // rfClass.def(py::init());
+    m.def("makeEngine", &makeEngineFromDict,
+        py::arg("userinformation"),
+        py::arg("system"),
+        py::arg("projections"),
+        py::arg("matrix"));
+
+    m.def("makeEngine", &makeEngineFromXML, py::arg("xml_file"), py::arg("project_name"));
+    // py::arg("processchain")
+    // m.def("Potatoes", []() {
+    //     std::cout << "I am here\n";
+    // });
+}
 
 //    sfClass.def("configure",
 //                &ImagingAlgorithms::StripeFilter::configure,
@@ -153,5 +227,4 @@ void bindReconFactory(py::module &m)
 
 
 
-}
-#endif
+// #endif
--- frameworks/tomography/Framework/ReconFramework/src/pybinder.cpp
+++ frameworks/tomography/Framework/ReconFramework/src/pybinder.cpp
@@ -1,21 +1,22 @@
-#ifdef HAVEPYBIND11
-#include <pybind11/pybind11.h>
+#include <iostream>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "../include/ReconException.h"
-
 namespace py = pybind11;
-
 void bindReconFactory(py::module &m);
-void bindReconEngine(py::module &m);
+//void bindPolynomialCorrection(py::module &m);
+//void bindMorphSpotClean(py::module &m);
+//void bindStripeFilter(py::module &m);
+//void bindPixelInfo(py::module &m);
+//void bindException(py::module &m);
 
-PYBIND11_MODULE(reconframework,m)
+PYBIND11_MODULE(reconframework, m)
 {
     bindReconFactory(m);
-    bindReconEngine(m);
 
-    py::register_exception<ReconException>(m, "ReconException");
+    //    bindPixelInfo(m);
+    //    bindException(m);
+
+    //    py::register_exception<ImagingException>(m, "ImagingException");
 }
-
-#endif
