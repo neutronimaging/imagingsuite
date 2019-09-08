f73d13ff8fd9e76a35b44e82394cba219262ed6a
Anders Kaestner <anders.kaestner@psi.ch>
2019-09-08 18:22:06 +0200

    Adding updated exception base

--- core/kipl/kipl/include/base/KiplException.h
+++ core/kipl/kipl/include/base/KiplException.h
@@ -15,25 +15,26 @@ public :
     /// \brief Basic exception C'tor. Only creates a message.
     /// \param message The error message that is carried by the exception
     /// \param exname Name of the exception. This is to be assigned by the C'tor of the deriving class
-    KiplException(std::string message, std::string exname="KiplException");
+    KiplException(const std::string &message, const std::string &exname="KiplException");
 
     /// \brief Exception C'tor with full information.
     /// \param message The error message that is carried by the exception
     /// \param filename Name of the file that throws the exception (provided by __FILE__)
     /// \param linenumber line number of the location where the exception was thrown.
     /// \param exname Name of the exception. This is to be assigned by the C'tor of the deriving class
-    KiplException(std::string message, std::string filename, size_t linenumber, std::string exname="KiplException");
+    KiplException(const std::string & message, const std::string & filename, size_t linenumber, const std::string &exname="KiplException");
 	virtual ~KiplException();
 
     /// \brief Getter for the error message
     /// \returns A string containing the error message information
     //std::string what() const;
-    const char* what() const;
+    virtual const char* what() const ;
 protected :
     std::string sExceptionName;
 	std::string sMessage;
 	std::string sFileName;
 	size_t nLineNumber;
+    std::string sFormattedMessage;
 	
 };
 
