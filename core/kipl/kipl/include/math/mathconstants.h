7df4e0e68ccdfa76bf6d98b726d09d5dd2530bbf
chiaracarminati <chiara.carminati@psi.ch>
2019-11-15 16:21:13 +0100

    corrected square root of 2

--- core/kipl/kipl/include/math/mathconstants.h
+++ core/kipl/kipl/include/math/mathconstants.h
@@ -2,12 +2,13 @@
 
 #ifndef MATHCONSTANTS_H_
 #define MATHCONSTANTS_H_
+#include <math.h>
 
 const long double  lPi  = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214l;
 const double       dPi  = 3.1415926535897932384626433832795028841971693993751;
 const float        fPi  = 3.1415926535897932384626f;
-const double       dsqrt2 = 1.4142129629629629629629629629629629629629629629629;
-const float        fsqrt2 = 1.4142129626926926926926f;
+const double       dsqrt2 = sqrt(2.0);
+const float        fsqrt2 = sqrt(2.0f);
 const double       dnmass = 1.674927471e-27;
 const double       dplanck = 6.62607004e-34;
 
