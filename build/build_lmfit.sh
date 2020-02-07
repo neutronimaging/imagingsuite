169e37604112c7253372b55f96df4dba1519f2a8
Chiara <carminati@ldmadmins-MBP.psi.ch>
2020-02-07 10:24:39 +0100

    changed exit 1 to exit 0 for cmake existance check while building lmfit

--- build/build_lmfit.sh
+++ build/build_lmfit.sh
@@ -7,7 +7,7 @@ mkdir $WORKSPACE/imagingsuite/external/src/lmfit/build
 mkdir $DEST_DEPENDENCY
 cd $WORKSPACE/imagingsuite/external/src/lmfit/build
 
-command -v cmake >/dev/null 2>&1 || { echo >&2 "I require cmake but it's not installed.  Aborting."; exit 1; }
+command -v cmake >/dev/null 2>&1 || { echo >&2 "I require cmake but it's not installed.  Skipping."; exit 0; }
 
 cmake -DCMAKE_INSTALL_PREFIX=$DEST_DEPENDENCY .. 
 make
