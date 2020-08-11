4b0269b51f88410af16fac950386fa8974ccd003
Anders Kaestner <anders.kaestner@psi.ch>
2020-08-11 15:05:44 +0200

    Fixed error sanity check method.

--- frameworks/tomography/Framework/ReconFramework/src/ReconConfig.cpp
+++ frameworks/tomography/Framework/ReconFramework/src/ReconConfig.cpp
@@ -69,7 +69,7 @@ bool ReconConfig::SanityAnglesCheck()
 {
     std::ostringstream msg;
 
-    if ( ((ProjectionInfo.scantype==ProjectionInfo.GoldenSectionScan) || (ProjectionInfo.scantype==ProjectionInfo.GoldenSectionScan))
+    if ( ((ProjectionInfo.scantype==ProjectionInfo.GoldenSectionScan) || (ProjectionInfo.scantype==ProjectionInfo.InvGoldenSectionScan))
             && (ProjectionInfo.fScanArc[1]!=180.0f && ProjectionInfo.fScanArc[1]!=360.0f))
     {
         msg<<"Incorrect angles configuration " ;
