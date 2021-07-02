2055f3c2db44779e1f2aaa86b4d582b5069cc11f
Anders Kaestner <anders.kaestner@psi.ch>
2021-07-02 14:49:44 +0200

    FDK backprojectors compile with cmake

--- frameworks/tomography/Backprojectors/FDKBackProjectors/src/fdkbackproj_global.h
+++ frameworks/tomography/Backprojectors/FDKBackProjectors/src/fdkbackproj_global.h
@@ -3,16 +3,18 @@
 #ifndef FDKBACKPROJ_GLOBAL_H
 #define FDKBACKPROJ_GLOBAL_H
 
-#ifndef NO_QT
-#include <QtCore/qglobal.h>
+#if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#  define Q_DECL_EXPORT __declspec(dllexport)
+#  define Q_DECL_IMPORT __declspec(dllimport)
+#else
+#  define Q_DECL_EXPORT     __attribute__((visibility("default")))
+#  define Q_DECL_IMPORT     __attribute__((visibility("default")))
+#endif
 
 #if defined(FDKBACKPROJ_LIBRARY)
 #  define FDKBACKPROJSHARED_EXPORT Q_DECL_EXPORT
 #else
 #  define FDKBACKPROJSHARED_EXPORT Q_DECL_IMPORT
-#endif
-#else
-#  define FDKBACKPROJSHARED_EXPORT
 #endif
 
 #ifdef __GNUC__
