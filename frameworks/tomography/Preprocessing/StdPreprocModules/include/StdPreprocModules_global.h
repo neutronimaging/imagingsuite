b0b3ae09bfb24b20401e2b5d750aca38228ec54f
Anders Kaestner <anders.kaestner@psi.ch>
2022-04-14 16:30:19 +0200

    Got two more Cmakes running and added one.

--- frameworks/tomography/Preprocessing/StdPreprocModules/include/StdPreprocModules_global.h
+++ frameworks/tomography/Preprocessing/StdPreprocModules/include/StdPreprocModules_global.h
@@ -1,7 +1,13 @@
 #ifndef STDPREPROCMODULES_GLOBAL_H
 #define STDPREPROCMODULES_GLOBAL_H
 
-#include <QtCore/qglobal.h>
+#if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#  define Q_DECL_EXPORT __declspec(dllexport)
+#  define Q_DECL_IMPORT __declspec(dllimport)
+#else
+#  define Q_DECL_EXPORT     __attribute__((visibility("default")))
+#  define Q_DECL_IMPORT     __attribute__((visibility("default")))
+#endif
 
 #if defined(STDPREPROCMODULES_LIBRARY)
 #  define STDPREPROCMODULESSHARED_EXPORT Q_DECL_EXPORT
