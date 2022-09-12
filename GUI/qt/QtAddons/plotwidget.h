9980466a42b3f5a70aa5cabca44214b8814ab636
Anders Kaestner <anders.kaestner@psi.ch>
2022-09-12 11:20:36 +0200

    Added single vector interface to the plot widget.

--- GUI/qt/QtAddons/plotwidget.h
+++ GUI/qt/QtAddons/plotwidget.h
@@ -26,6 +26,7 @@ public:
     ~PlotWidget();
 
     void setCurveData(int id, const QVector<QPointF> &data, QString name = "");
+    void setCurveData(int id, const std::vector<float> &data, QString name = "");
     void setCurveData(int id, const std::vector<float> &x, const std::vector<float> &y, QString name = "");
     void setCurveData(int id, const std::map<float,float> &data, QString name = "");
     void setCurveData(int id, float const * const x, float const * const y, const int N, QString name = "");
