afc8c84a33f5b3d4e27dd32595d622ed1e0d8766
Anders Kaestner <anders.kaestner@psi.ch>
2023-10-17 11:17:37 +0200

    Updated default parameters for WaveletRingClean

--- frameworks/tomography/Preprocessing/StdPreprocModules/src/WaveletRingClean.cpp
+++ frameworks/tomography/Preprocessing/StdPreprocModules/src/WaveletRingClean.cpp
@@ -16,8 +16,8 @@
 WaveletRingClean::WaveletRingClean(kipl::interactors::InteractionBase *interactor) :
     PreprocModuleBase("WaveletRingClean",interactor),
     m_sWName("daub15"),
-    m_fSigma(0.05f),
-    m_nDecNum(2),
+    m_fSigma(0.01f),
+    m_nDecNum(4),
 	m_eCleanMethod(ImagingAlgorithms::VerticalComponentFFT)
 {
     m_bThreading = true;
