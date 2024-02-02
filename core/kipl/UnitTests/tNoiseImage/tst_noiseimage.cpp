e869c56130775e68dd0f2878e7840aea272860c0
Anders Kaestner <anders.kaestner@psi.ch>
2024-02-02 12:31:37 +0100

    Added noise generator and test

--- core/kipl/UnitTests/tNoiseImage/tst_noiseimage.cpp
+++ core/kipl/UnitTests/tNoiseImage/tst_noiseimage.cpp
@@ -3,6 +3,8 @@
 
 #include <base/timage.h>
 #include <generators/NoiseImage.h>
+#include <math/statistics.h>
+#include <utilities/compare.h>
 
 class TNoiseImageTest : public QObject
 {
@@ -27,10 +29,17 @@ void TNoiseImageTest::testGauss()
     std::vector<size_t> dims={1000,1000};
 
     kipl::base::TImage<float,2> img(dims);
+    img = 0.0f;
 
     kipl::generators::NoiseGenerator<float,2> noise;
 
-   // noise.Gauss(img,10.0f,1.0f);
+    noise.Gauss(img,10.0f,1.0f);
+
+    kipl::math::Statistics stats;
+    stats.put(img.GetDataPtr(),img.Size());
+
+    QVERIFY2(fuzzyRelativeCompare(stats.s(),1.0,0.001),  "Standard deviation is not correct ");
+    QVERIFY2(fuzzyRelativeCompare(stats.E(),10.0,0.001), "Mean is not correct ");
 }
 
 void TNoiseImageTest::testPoisson()
@@ -40,4 +49,4 @@ void TNoiseImageTest::testPoisson()
 
 QTEST_APPLESS_MAIN(TNoiseImageTest)
 
-#include "tst_tnoiseimagetest.moc"
+#include "tst_noiseimage.moc"
