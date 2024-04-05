279c4b0d7a1e1c736391c51a59bbf2a464b96443
Christian Vedel <christian.vedel@ess.eu>
2024-04-05 09:48:16 +0200

    conan test

--- conanfile.py
+++ conanfile.py
@@ -47,10 +47,13 @@ class MuhrecRecipe(ConanFile):
             if len(dep.cpp_info.bindirs)>0: # Avoid errors when using header-only files such as dirent. Can probably be done neater
                 copy(self, "*.dll", dep.cpp_info.bindirs[0], bin_folder)
                 copy(self, "*.dylib", dep.cpp_info.bindirs[0], bin_folder)
+                print('bindir:')
+                print(os.listdir(dep.cpp_info.bindirs[0]))
             if len(dep.cpp_info.libdirs)>0:
                 copy(self, "*.so*", dep.cpp_info.libdirs[0], lib_folder)
-            print(os.listdir(dep.cpp_info.bindirs[0]))
-            print(os.listdir(dep.cpp_info.libdirs[0]))
+                print('libdir:')
+                print(os.listdir(dep.cpp_info.libdirs[0]))
+            
         # Copy dynamic libraries from qt
         qtpath = os.environ["QTPATH"]
         Qt_dynamic_library_list = ["Qt6PrintSupport", "Qt6Charts", "Qt6OpenGLWidgets", "Qt6OpenGl", "Qt6Test"]
