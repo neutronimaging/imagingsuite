48d43f193fe35fef8953714b4dc3605f9e693d6b
Anders Kaestner <anders.kaestner@psi.ch>
2024-04-12 09:41:57 +0200

    Added missing files

---
+++ core/kipl/UnitTests/tKIPLHistogram/tst_histogram.cpp
@@ -0,0 +1,82 @@
+#include <QString>
+#include <QtTest>
+
+#include <base/timage.h>
+#include <base/thistogram.h>
+#include <generators/NoiseImage.h>
+
+class HistogramTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    HistogramTest();
+
+private Q_SLOTS:
+    void testBaselineVsOpt();
+    void benchmarkBaseline();
+    void benchmarkOpt();
+
+private:
+    void makeTestImage();
+    kipl::base::TImage<float,3> img;
+
+};
+
+HistogramTest::HistogramTest()
+{
+    makeTestImage();
+}
+
+void HistogramTest::makeTestImage()
+{
+    img = kipl::base::TImage<float,3>({1000,1000,10});
+    kipl::generators::NoiseGenerator<float,3> noise;
+    noise.Gauss(img, 0.0, 1.0);
+}
+
+void HistogramTest::testBaselineVsOpt()
+{
+    const size_t nBins = 500;
+    std::vector<size_t> hist(nBins);
+    std::vector<float> axis(nBins);
+    kipl::base::Histogram(img.GetDataPtr(), img.Size(), nBins, hist, axis);
+    
+    std::vector<size_t> hist2(nBins);
+    std::vector<float> axis2(nBins);
+    kipl::base::HistogramOpt(img.GetDataPtr(), img.Size(), nBins, hist2, axis2);
+
+    for (size_t i = 0; i < nBins; ++i)
+    {
+        QCOMPARE(hist[i],hist2[i]);
+        QCOMPARE(axis[i],axis2[i]);
+    }
+}
+
+void HistogramTest::benchmarkBaseline()
+{
+    QBENCHMARK
+    {
+        const size_t nBins = 500;
+        std::vector<size_t> hist(nBins);
+        std::vector<float> axis(nBins);
+        kipl::base::Histogram(img.GetDataPtr(), img.Size(), nBins, hist, axis,-2.0,2.0,true);
+    }
+}
+
+void HistogramTest::benchmarkOpt()
+{
+    QBENCHMARK
+    {
+        const size_t nBins = 500;
+        std::vector<size_t> hist(nBins);
+        std::vector<float> axis(nBins);
+        kipl::base::HistogramOpt(img.GetDataPtr(), img.Size(), nBins, hist, axis,-2.0,2.0,true,4192);
+    }
+}
+
+QTEST_APPLESS_MAIN(HistogramTest)
+
+#include "tst_histogram.moc"
+
+
