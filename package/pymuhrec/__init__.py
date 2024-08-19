76fb975826a2da46ca2f40203ff1a0bd41c1a244
Christian Vedel <christian.vedel@ess.eu>
2024-08-19 10:23:18 +0200

    Add utilities to pymuhrec

--- package/pymuhrec/__init__.py
+++ package/pymuhrec/__init__.py
@@ -6,6 +6,6 @@ cur_file_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(cur_file_dir)
 os.chdir(cur_file_dir)
 
-from advfilters import *
-from imgalg import *
-from muhrectomo import *
+import advfilters
+import imgalg
+import muhrectomo
