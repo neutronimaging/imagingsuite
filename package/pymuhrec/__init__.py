af5a3a93714b0ce2d2a472b8c717f43b4ccc4737
Christian Vedel <christian.vedel@ess.eu>
2024-08-20 10:00:10 +0200

    Add test examples and update notebooks

--- package/pymuhrec/__init__.py
+++ package/pymuhrec/__init__.py
@@ -6,6 +6,7 @@ cur_file_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(cur_file_dir)
 os.chdir(cur_file_dir)
 
-import advfilters
-import imgalg
-import muhrectomo
+from advfilters import *
+from muhrectomo import *
+from imgalg import *
+
