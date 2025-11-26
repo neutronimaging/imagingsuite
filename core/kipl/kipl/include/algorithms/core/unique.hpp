2d497bc64015bf5fe5332ee3c9d91c06533687d7
Anders Kaestner <anders.kaestner@psi.ch>
2025-11-26 15:05:27 +0100

    Adding unique function

---
+++ core/kipl/kipl/include/algorithms/core/unique.hpp
@@ -0,0 +1,21 @@
+#if !defined(UNIQUE_HPP)
+#define UNIQUE_HPP
+
+#include <vector>
+#include <algorithm>
+#include <numeric>   // std::iota
+#include <iterator>  // std::distance
+
+namespace kipl {
+namespace algorithms {
+    
+template<class T>
+std::vector<T> unique_values(std::vector<T> v)
+{
+    std::sort(v.begin(), v.end());
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+    return v;
+}
+
+}}
+#endif
