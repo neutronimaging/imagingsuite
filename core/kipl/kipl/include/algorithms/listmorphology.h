2858d6399daf1656c04e0f7f7a707d5526d23f53
Anders Kaestner <anders.kaestner@psi.ch>
2026-05-18 17:50:09 +0200

    Added algorithim to dilate pixels stored in a list. Small unit test seems to work for 4- and 8-connectivity.

---
+++ core/kipl/kipl/include/algorithms/listmorphology.h
@@ -0,0 +1,23 @@
+//<LICENCE>
+#ifndef UNIQUE_H
+#define UNIQUE_H
+
+#include <vector>
+
+#include <base/kiplenums.h>
+
+namespace kipl {
+namespace algorithms {
+
+
+/// @brief Returns a vector containing the unique values from the input vector. 
+/// @tparam T Data type of the vector elements.  
+/// @param v Input vector.
+/// @return A vector containing the unique values from the input vector.
+template<class T>
+std::vector<T> dilate_points(std::vector<T> &v, const std::vector<T>& dims, kipl::base::eConnectivity conn);
+
+}}
+#endif
+
+#include "core/listmorphology.hpp"
